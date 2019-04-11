351f9d791ead4544d6c949d98e32cd0222cac9c2
aparna <aparna16itech@gmail.com>
2019-04-12 01:24:38 +0530

    initial commit for react basics

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Age:{this.props.age}</h1>
+        <button onClick={this.props.onAgeUp}>Age up</button>....
+        <button onClick={this.props.onAgeDown}>Age down</button>
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return{
+    age:state.age
+  }
+};
+const mapDispatchToProps = (dispatch) => {
+  return{
+  onAgeUp: () => dispatch({type:'AGE_UP'}),
+  onAgeDown: () => dispatch({type:'AGE_DOWN'})
+  }
+};
+export default connect(mapStateToProps,mapDispatchToProps)(App);
