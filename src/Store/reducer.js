02ec905365c01f8fbbb1e1c45c21716993190f32
aparna <aparna16itech@gmail.com>
2019-04-12 03:03:39 +0530

    improved code

--- src/Store/reducer.js
+++ src/Store/reducer.js
@@ -1,13 +1,13 @@
 const initialState = {
-    age:21
+    age: 21
 }
 
-const reducer = (state=initialState, action) => {
-    const newState= { ...state }
-    if(action.type === 'AGE_UP'){
+const reducer = (state = initialState, action) => {
+    const newState = { ...state }
+    if (action.type === 'AGE_UP') {
         newState.age++;
     }
-    if(action.type === 'AGE_DOWN'){
+    if (action.type === 'AGE_DOWN') {
         newState.age--;
     }
     return newState;
